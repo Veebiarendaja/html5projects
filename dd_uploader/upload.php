d5de8862ed86137c0ee2c49442d3c436eb0fb064
Evgeniy Kureyko <evgeniy.kureyko@khk.ee>
2015-11-24 01:30:16 +0200

    Lecture 42: PHP upload file Part - B [Deliver #108717764]

--- dd_uploader/upload.php
+++ dd_uploader/upload.php
@@ -2,12 +2,12 @@
 $upload_dir = 'uploads/';
 $allowed_ext = array('jpg', 'jpeg', 'png', 'gif');
 
-if (array_key_exists('pic', $_FILES && $_FILES['pic']['error'] == 0)) {
+if (array_key_exists('pic', $_FILES) && $_FILES['pic']['error'] == 0) {
     $pic = $_FILES['pic'];
     if (!in_array(get_extension($pic['name']), $allowed_ext)) {
         exit_status('Only ' . implode(',', $allowed_ext) . ' files are allowed');
     }
-    if (move_uploaded_files($pic['tmp_name'], $upload_dir . $pic['name'])) {
+    if (move_uploaded_file($pic['tmp_name'], $upload_dir . $pic['name'])) {
         exit_status('File uploaded');
     }
 }
@@ -16,5 +16,12 @@ if (array_key_exists('pic', $_FILES && $_FILES['pic']['error'] == 0)) {
 
 function exit_status($str) {
     echo json_encode(array('status'=>$str));
+    exit;
+}
+
+function get_extension($file) {
+    $ext = explode('.',$file);
+    $ext = array_pop($ext);
+    return strtolower($ext);
 }
 ?>
