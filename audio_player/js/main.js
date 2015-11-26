bea31023d0eb6460fae2cef320a7d60bfbbe7d31
Evgeniy Kureyko <evgeniy.kureyko@khk.ee>
2015-11-26 22:59:59 +0200

    Lecture 54: Play Pause and Stop [Deliver #109040562]

--- audio_player/js/main.js
+++ audio_player/js/main.js
@@ -1,3 +1,20 @@
-/**
- * Created by Senka on 26.11.2015.
- */
+var audio = new Audio('media/Linkin Park - By Myself.mp3');
+
+$('#pause').hide();
+
+$('#play').click(function(){
+    audio.play();
+    $('#pause').show();
+    $('#play').hide();
+});
+
+$('#pause').click(function(){
+    audio.pause();
+    $('#play').show();
+    $('#pause').hide();
+});
+
+$('#stop').click(function() {
+   audio.pause();
+    audio.currentTime = 0;
+});
