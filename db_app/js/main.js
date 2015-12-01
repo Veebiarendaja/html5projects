54bf5a53f2bb10142821980152af80aadfc6bab6
Evgeniy Kureyko <evgeniy.kureyko@khk.ee>
2015-12-01 22:37:11 +0200

    Lecture 71: Update Customers [Deliver #109216314]

--- db_app/js/main.js
+++ db_app/js/main.js
@@ -1,26 +1,26 @@
-$(document).ready(function() {
+$(document).ready(function () {
     //Open Database
-    var request = indexedDB.open('customermanager',1);
+    var request = indexedDB.open('customermanager', 1);
 
     request.onupgradeneeded = function (e) {
         var db = e.target.result;
 
-        if(!db.objectStoreNames.contains('customers')) {
-            var os = db.createObjectStore('customers',{keyPath: "id", autoIncrement:true});
+        if (!db.objectStoreNames.contains('customers')) {
+            var os = db.createObjectStore('customers', {keyPath: "id", autoIncrement: true});
             //Create Index for name
-            os.createIndex('name','name',{unique:false});
+            os.createIndex('name', 'name', {unique: false});
         }
     }
 
     //Success
-    request.onsuccess = function(e) {
+    request.onsuccess = function (e) {
         console.log('Success: Opened Database');
         db = e.target.result;
         //Show Customers
         showCustomers();
     }
     //Error
-    request.onerror = function(e) {
+    request.onerror = function (e) {
         console.log('Error');
 
     }
@@ -32,7 +32,7 @@ function addCustomer() {
     var name = $('#name').val();
     var email = $('#email').val();
 
-    var transaction = db.transaction(["customers"],"readwrite");
+    var transaction = db.transaction(["customers"], "readwrite");
     //Ask for ObjectStore
     var store = transaction.objectStore("customers");
 
@@ -47,35 +47,34 @@ function addCustomer() {
     var request = store.add(customer);
 
     //Success
-    request.onsuccess = function(e) {
-        window.location.href="index.html";
+    request.onsuccess = function (e) {
+        window.location.href = "index.html";
     }
 
     //Error
-    request.onerror = function(e) {
+    request.onerror = function (e) {
         alert("Sorry, the customer was not added");
         console.log('Error', e.target.error.name);
     }
 }
 
 //Display Customers
-function showCustomers(e){
-    var transaction = db.transaction(["customers"],"readonly");
+function showCustomers(e) {
+    var transaction = db.transaction(["customers"], "readonly");
     //Ask for ObjectStore
     var store = transaction.objectStore("customers");
     var index = store.index('name');
 
     var output = '';
-    index.openCursor().onsuccess = function(e) {
+    index.openCursor().onsuccess = function (e) {
         var cursor = e.target.result;
-        if(cursor){
-            output += "<tr id='customer_"+cursor.value.id+"'>";
-            output += "<td>"+cursor.value.id+"<td>";
-            output += "<td><span>"+cursor.value.name+"</span><td>";
-            output += "<td><span>"+cursor.value.email+"</span><td>";
-            output += "<td><a onclick='removeCustomer("+cursor.value.id+")' href=''>Delete</a><td>";
+        if (cursor) {
+            output += "<tr id='customer_" + cursor.value.id + "'>";
+            output += "<td>" + cursor.value.id + "</td>";
+            output += "<td><span class='cursor customer' contenteditable='true' data-field='name' data-id='" + cursor.value.id + "'>" + cursor.value.name + "</span></td>";
+            output += "<td><span class='cursor customer' contenteditable='true' data-field='email' data-id='" + cursor.value.id + "'>" + cursor.value.email + "</span></td>";
+            output += "<td><a onclick='removeCustomer(" + cursor.value.id + ")' href=''>Delete</a></td>";
             output += "</tr>";
-            cursor.continue();
         }
         $('#customers').html(output);
     }
@@ -83,19 +82,19 @@ function showCustomers(e){
 
 //Delete a Customer
 function removeCustomer(id) {
-    var transaction = db.transaction(["customers"],"readwrite");
+    var transaction = db.transaction(["customers"], "readwrite");
     var store = transaction.objectStore("customers");
 
     var request = store.delete(id);
 
     //Success
-    request.onsuccess = function() {
-        console.log('customer '+id+' deleted');
-        $('customer_'+id).remove();
+    request.onsuccess = function () {
+        console.log('customer ' + id + ' deleted');
+        $('customer_' + id).remove();
     }
 
     //Error
-    request.onerror = function(e) {
+    request.onerror = function (e) {
         alert("Sorry, the customer was not removed");
         console.log('Error', e.target.error.name);
     }
@@ -103,5 +102,35 @@ function removeCustomer(id) {
 
 function clearCustomers() {
     indexedDB.deleteDatabase('customermanager');
-    window.location.href="index.html";
+    window.location.href = "index.html";
 }
+
+//Update Customer
+$('#customers').on('blur', '.customer', function () {
+    var newText = $(this).html();
+    var field = $(this).data('field'); //data-field html5
+    var id = $(this).data('id');
+
+    var transaction = db.transaction(["customers"], "readwrite");
+    var store = transaction.objectStore("customers");
+
+    var request = store.get(id);
+
+    request.onsuccess = function () {
+        var data = request.result;
+
+        data[field] = newText;
+
+        //Store Updated Text
+        var requestUpdate = store.put(data);
+
+        requestUpdate.onsuccess = function () {
+            console.log('Customer filed updated');
+        }
+
+        requestUpdate.onerror = function () {
+            console.log('Error');
+        }
+    }
+
+});
